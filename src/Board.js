b53d4b82901054183abd1a4979f83c85cab300bb
Tate Wilhelm <tatewilhelm@yahoo.com>
2020-08-14 13:09:44 -0400

    Add rows

--- src/Board.js
+++ src/Board.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Grid from '@material-ui/core/Grid';
 
 
 
@@ -15,8 +16,39 @@ export class Board extends Component {
     createCells() {
         for (let i = 0; i < 20; i++) { // magic #
             for (let j = 0; j < 20; j++) {
-                <Cell xPos={i} yPos={j}></Cell>
+
+                    <Cell xPos={i} yPos={j}></Cell>
             }
         }
+    }
+
+    createRows(rowNum) {
+        <React.Fragment>
+            <Grid item>
+                <Cell xPos={0} yPos={rowNum}></Cell>
+            </Grid>
+            <Grid item>
+                <Cell xPos={1} yPos={rowNum}></Cell>
+            </Grid>
+            <Grid item>
+                <Cell xPos={2} yPos={rowNum}></Cell>
+            </Grid>
+            <Grid item>
+                <Cell xPos={3} yPos={rowNum}></Cell>
+            </Grid>
+            <Grid item>
+                <Cell xPos={4} yPos={rowNum}></Cell>
+            </Grid>
+        </React.Fragment>
+    }
+
+    render() {
+        return (
+            <Grid container>
+                <Grid container item>
+                    {this.createRows(0)}
+                </Grid>
+            </Grid>
+        );
     }
 }
