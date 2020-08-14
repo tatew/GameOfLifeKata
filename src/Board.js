5c386bd1cddeed8d9aba87b80cca2b18f84a10e5
lrose07 <larose@radford.edu>
2020-08-14 13:27:05 -0400

    add grid locations

--- src/Board.js
+++ src/Board.js
@@ -1,52 +1,65 @@
 import React, {Component} from 'react'
 import Grid from '@material-ui/core/Grid';
+import {Cell} from "./Cell";
 
 
 
 export class Board extends Component {
     constructor(props) {
         super(props);
-
-        this.createCells();
     }
 
     // nested for loop to create cells, cell locations i,j >> new Cell(i, j)
     // cell constructor
 
-    createCells() {
-        for (let i = 0; i < 20; i++) { // magic #
-            for (let j = 0; j < 20; j++) {
-
-                    <Cell xPos={i} yPos={j}></Cell>
-            }
-        }
-    }
+    // createCells() {
+    //     for (let i = 0; i < 20; i++) { // magic #
+    //         for (let j = 0; j < 20; j++) {
+    //
+    //                 <Cell xPos={i} yPos={j}></Cell>
+    //         }
+    //     }
+    // }
 
     createRows(rowNum) {
-        <React.Fragment>
-            <Grid item>
-                <Cell xPos={0} yPos={rowNum}></Cell>
-            </Grid>
-            <Grid item>
-                <Cell xPos={1} yPos={rowNum}></Cell>
-            </Grid>
-            <Grid item>
-                <Cell xPos={2} yPos={rowNum}></Cell>
-            </Grid>
-            <Grid item>
-                <Cell xPos={3} yPos={rowNum}></Cell>
-            </Grid>
-            <Grid item>
-                <Cell xPos={4} yPos={rowNum}></Cell>
-            </Grid>
-        </React.Fragment>
+        return (
+            <React.Fragment>
+                <Grid item>
+                    <Cell row={rowNum} col={0} state={0}></Cell>
+                </Grid>
+                <Grid item>
+                    <Cell row={rowNum} col={1} state={0}></Cell>
+                </Grid>
+                <Grid item>
+                    <Cell row={rowNum} col={2} state={0}></Cell>
+                </Grid>
+                <Grid item>
+                    <Cell row={rowNum} col={3} state={0}></Cell>
+                </Grid>
+                <Grid item>
+                    <Cell row={rowNum} col={4} state={0}></Cell>
+                </Grid>
+            </React.Fragment>
+        )
     }
 
     render() {
         return (
-            <Grid container>
-                <Grid container item>
+            <Grid container spacing={3}>
+                <Grid container item spacing={3}>
                     {this.createRows(0)}
+                </Grid>
+                <Grid container item spacing={3}>
+                    {this.createRows(1)}
+                </Grid>
+                <Grid container item spacing={3}>
+                    {this.createRows(2)}
+                </Grid>
+                <Grid container item spacing={3}>
+                    {this.createRows(3)}
+                </Grid>
+                <Grid container item spacing={3}>
+                    {this.createRows(4)}
                 </Grid>
             </Grid>
         );
--- src/Cell.js
+++ src/Cell.js
@@ -9,6 +9,12 @@ export class Cell extends Component {
 
 
     }
+
+    render() {
+        return (
+            <p>{this.props.row}, {this.props.col}</p>
+        )
+    }
 }
 
 // cell knows its location, queries model for the state at that location, updates its state to match
